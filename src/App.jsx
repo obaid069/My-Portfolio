0c63c7e4597b5d643cff01421b259aa38fb8a3e1
obaid069 <obaidzafar234@gmail.com>
2026-01-28 11:15:23 +0500

    Add Experience section to App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './components/sections/Hero';
 import About from './components/sections/About';
 import Skills from './components/sections/Skills';
 import Projects from './components/sections/Projects';
+import Experience from './components/sections/Experience';
 import Contact from './components/sections/Contact';
 import './App.css';
 
@@ -19,6 +20,7 @@ function App() {
           <About />
           <Skills />
           <Projects />
+          <Experience />
           <Contact />
         </main>
         <Footer />
